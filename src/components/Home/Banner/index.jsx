cda609c75087c9deb5194445241413dc9e8646db
Abhinandan Jain <abhinandanbansal123@gmail.com>
2022-02-10 02:26:50 +0530

    project upload

---
+++ src/components/Home/Banner/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles.css';
+import bannerImg from './banner.png';
+import Logo from '../../common/Logo';
+
+const Banner = ({ handleScrollMenu }) => (
+  <header>
+    <div className='header-content'>
+      <Logo />
+      <div className='content-main'>
+        <h1>Delicious food waiting for you</h1>
+        <p>We make fresh and healthy meals with jhakass recipes</p>
+        <button onClick={handleScrollMenu}>
+          View Menu <i className='fas fa-long-arrow-alt-right'></i>
+        </button>
+      </div>
+    </div>
+    <img className='header-img' src={bannerImg} alt='banner' />
+  </header>
+);
+
+export default Banner;
---
+++ src/components/common/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from '../Logo';
+import './styles.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        We're foodies and we serve the best(created by abhiiiijain).....
+      </p>
+      <Logo />
+    </footer>
+  );
+};
+
+export default Footer;
